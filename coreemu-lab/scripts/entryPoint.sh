3960bff58213156fedad501e5819a091eb7a143c
Thomas Schmitt <t.e.schmitt@posteo.de>
2022-08-05 21:00:02 +0200

    :sparkles: first draft of experiment setup

---
+++ coreemu-lab/scripts/entryPoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+systemctl enable ssh
+service ssh start
+#service core-daemon start
+/update-custom-serivces.sh
+
+core-daemon > /var/log/core-daemon.log 2>&1 &
+
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+if [ ! -z "$SSHKEY" ]; then
+	echo "Adding ssh key: $SSHKEY"
+	mkdir /root/.ssh
+	chmod 755 ~/.ssh
+	echo $SSHKEY > /root/.ssh/authorized_keys
+    chmod 644 /root/.ssh/authorized_keys	
+fi
+
+FILE=/shared/experiment.conf
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+	sleep 1
+	time core-experiment $FILE 2>&1 | tee /shared/experiment.log
+else 
+	core-pygui
+fi
---
+++ coreemu-lab/scripts/start-monntpy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /app/moNNT.py
+poetry run python main.py
