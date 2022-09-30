a4686e5e95bccb180ca746544bddda1e733f4847
Thomas Schmitt <t.e.schmitt@posteo.de>
2022-09-30 17:51:10 +0200

    smaller changes

--- net-emu/build.sh
+++ net-emu/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# rsync -aPz --exclude '*.pyc' --exclude '__pycache__' --exclude ".git" --exclude ".idea" --exclude ".ipynb_checkpoints" \
-#     /home/thomas/thesis/moNNT.py .
+rsync -aPz --exclude '*.pyc' --exclude '__pycache__' --exclude ".git" --exclude ".idea" --exclude ".ipynb_checkpoints" \
+    /home/thomas/thesis/moNNT.py .
 # rsync -aPz --exclude '*.pyc' --exclude '__pycache__' --exclude ".git" --exclude ".idea" --exclude ".ipynb_checkpoints" \
 #     /home/thomas/thesis/py-dtn7 .
 
