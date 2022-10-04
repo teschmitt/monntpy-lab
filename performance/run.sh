c8687b3bf8f178471f65dd5a5c26d676ab458dcc
Thomas Schmitt <t.e.schmitt@posteo.de>
2022-10-04 17:37:13 +0200

    adds experiments run mode for statistical output

--- performance/run.sh
+++ performance/run.sh
@@ -1,25 +1,80 @@
 #!/bin/bash
 
-if [ -n "$1" ]; then
-    # if there is a number provided, change numer of messages to that number
-    num=$1
-else
-    num=100
-fi
+# Reset
+Color_Off='\033[0m'       # Text Reset
 
-if [ -n "$2" ]; then
-    # if there is a path provided, change path to the DB to that path
-    db_path="$2"
-else
-    db_path="/app/moNNT.py"
-fi
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+
+usage() {
+    echo   "Run performance tests with moNNT.py and dtnd"
+    echo
+    printf "${Yellow}USAGE:${Color_Off}\n"
+    echo   "    $cmd [OPTIONS]"
+    echo
+    printf "${Yellow}OPTIONS:${Color_Off}\n"
+    printf "    ${Green}-a${Color_Off}, ${Green}--num-articles${Color_Off}   Number of articles to send (single run mode only) [default: 100]\n"
+    printf "    ${Green}-d${Color_Off}, ${Green}--db-path${Color_Off}        Path to database [default: /app/moNNT.py]\n"
+    printf "    ${Green}-m${Color_Off}, ${Green}--run-mode${Color_Off}       Run mode: single or experiment [default: single]\n"
+    printf "    ${Green}-h${Color_Off}, ${Green}--help${Color_Off}           Show this message and exit\n"
+    echo
+}
+
+# Defaults:
+num_articles=100
+db_path="/app/moNNT.py"
+run_mode="single"
+cmd=$0
+
+
+while [[ "$#" -gt 0 ]]
+do case $1 in
+    -a|--num-articles)
+        num_articles="$2"
+        shift;;
+    -d|--db-path)
+        db_path="$2"
+        shift;;
+    -m|--run-mode)
+        run_mode="$2"
+        shift;;
+    -h|--help)
+        usage
+        exit 0;;
+    *)
+        printf "${BRed}error:${Color_Off} Unknown parameter passed: ${Yellow}'$1'${Color_Off}\n"
+        echo
+        usage
+        exit 1;;
+esac
+shift
+done
+
 
 docker run \
     --rm \
     --tty \
     --interactive \
-    --env NUM_ARTICLES=$num \
+    --env NUM_ARTICLES=$num_articles \
     --env DB_PATH=$db_path \
+    --env RUN_MODE=$run_mode \
     --volume $(pwd):/shared \
     --name monntpy-bench \
     monntpy-bench
